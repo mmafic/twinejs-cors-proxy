fdbfa3f91b0765b1b4e47e02b6f4e2fdf3ddc821
Monmusu Aficionado <monmusu.aficionado@gmail.com>
2023-11-28 15:01:35 -0500

    feat: restrict origin to twinery.org

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,20 +1,36 @@
+const MAX_AGE = '86400';
+
+const getOriginHeader = (request: Request) => {
+	const origin = request.headers.get('Origin');
+	if (origin) {
+		const url = new URL(origin);
+		if (url.hostname.endsWith('.twinery.org')) {
+			return url.origin;
+		}
+	}
+	return 'https://twinery.org';
+}
+
 // Respond to OPTIONS method
-export const onRequestOptions: PagesFunction = async () => {
+export const onRequestOptions: PagesFunction = async ({ request }) => {
+
 	return new Response(null, {
 		status: 204,
 		headers: {
-			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Origin': getOriginHeader(request),
 			'Access-Control-Allow-Headers': '*',
 			'Access-Control-Allow-Methods': 'GET, OPTIONS',
-			'Access-Control-Max-Age': '86400',
+			'Access-Control-Max-Age': MAX_AGE,
+			'Vary': 'Origin',
 		},
 	});
 };
 
 // Set CORS to all /api responses
-export const onRequest: PagesFunction = async ({ next }) => {
+export const onRequest: PagesFunction = async ({ request, next }) => {
 	const response = await next();
-	response.headers.set('Access-Control-Allow-Origin', '*');
-	response.headers.set('Access-Control-Max-Age', '86400');
+	response.headers.set('Access-Control-Allow-Origin', getOriginHeader(request));
+	response.headers.set('Access-Control-Max-Age', MAX_AGE);
+	response.headers.set('Vary', 'Origin');
 	return response;
 };
